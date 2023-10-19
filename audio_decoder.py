64c0e4da7a231cb41267c807e1b5195f3df29e35
Pengbo Wei <68414598+Samwei1@users.noreply.github.com>
2023-10-19 18:25:48 +0800

    add framework

--- audio_decoder.py
+++ audio_decoder.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 
 # Define the Transformer model
-class AudioGenerator(nn.Module):
+class Audio_Generator(nn.Module):
     def __init__(self, input_dim, output_dim, nhead, num_encoder_layers):
-        super(AudioGenerator, self).__init__()
+        super(Audio_Generator, self).__init__()
         self.transformer = nn.Transformer(
             d_model=input_dim,
             nhead=nhead,
--- model.py
+++ model.py
@@ -1,12 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from text_encoder import BERT_Encoder
+from audio_decoder import Audio_Generator
 
 # Define the Transformer model
-class Decoder(nn.Module):
+class Audio_Generator(nn.Module):
     def __init__(self, input_dim, output_dim, nhead, num_encoder_layers):
-        super(Decoder, self).__init__()
+        super(Audio_Generator, self).__init__()
+        self.text_encoder = BERT_Encoder(hidden=1000)
+        self.audio_generator = Audio_Generator(input_dim=1000, output_dim=output_dim)
         self.fc = nn.Linear(input_dim, output_dim)
 
-    def forward(self, src, mask):
-        return None
+    def forward(self, text):
+        text_embed = self.text_encoder(text)
+        audio = self.audio_generator(text_embed)
+        return audio
