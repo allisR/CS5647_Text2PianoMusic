b3c8fe609a5fb748f9c43c7f8a3c45bfb2e0cacb
“zxy91693” <zxy91693@163.com>
2023-10-23 05:27:41 +0800

    'dataloader'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 import json
 import numpy as np
 import torch
@@ -18,19 +19,22 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 from torch import nn
 from sklearn.metrics import f1_score
 from model import Audio_Generator
+from utils import *
 
 
 class ContextDataset(Dataset):
-    def __init__(self, x_inputs, tokenizer, max_len):
+    def __init__(self, x_inputs, tokenizer, max_len, audios):
         self.x = x_inputs
         self.tokenizer = tokenizer
         self.max_len = max_len
+        self.audios = audios
 
     def __len__(self):
         return len(self.x)
 
     def __getitem__(self, index):
         text = self.x[index]
+        audio = self.audios[index]
 
         embedding = self.tokenizer.encode_plus(
             text,
@@ -48,19 +52,75 @@ class ContextDataset(Dataset):
             "input_ids": embedding['input_ids'].flatten(),
             "attention_mask": embedding['attention_mask'].flatten(),
             "token_type_ids": embedding['token_type_ids'].flatten()
-        }
+        }, audio
 
-txt = ["I love", "You love me"]
+
+
+parser = argparse.ArgumentParser()
+# experimental settings
+parser.add_argument("--base_path",
+    type=str,
+    default="./maestro-v2.0.0/",
+    help="base_path of audio data")
+parser.add_argument("--csv_path", 
+    type=str,
+    default="./data/clean+GPTcaption.csv",
+    help="csv dataset path")
+parser.add_argument("--max_text_lenth", 
+    type=int,
+    default=512,
+    help="max_lenth for bert tokenizer")
+parser.add_argument("--batch_size", 
+    type=int,
+    default=5,
+    help="batch size for dataloader")
+parser.add_argument("--frame", 
+    type=int,
+    default=100,
+    help="#frame/sec for audio")
+args = parser.parse_args()
+
+base_path = args['base_path'] # './maestro-v2.0.0/'
+csv_path = args['csv_path'] # './data/clean+GPTcaption.csv'
+
+data = read_csv(csv_path)
+
+midi_files = get_midi_files(data,base_path)
+
+text_descriptions = get_text_descriptions(data)
+
+num_data = len(text_descriptions)
+train_num = int(num_data * 0.8)
+valid_num = int((num_data-train_num)/2)
+test_num = num_data - train_num - valid_num
+
+train_text = text_descriptions[:train_num] # [train_num]
+valid_text = text_descriptions[train_num:train_num+valid_num]
+test_text = text_descriptions[train_num+valid_num:]
+
+audios = midi_files_to_audios(midi_files,args['frame'])
+train_audios = audios[:train_num] # [train_num, frame*time]
+valid_audios = audios[train_num:train_num+valid_num]
+test_audios = audios[train_num+valid_num:]
+
+# txt = ["I love", "You love me"]
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-train_dataset = ContextDataset(txt, tokenizer, 5)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=1, shuffle=True)
+train_dataset = ContextDataset(train_text, tokenizer, args['max_text_lenth'] , train_audios)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size= args['batch_size'], shuffle=True)
 
-audio_example = torch.randint(0, 10, (1, 8)) # B S
+valid_dataset = ContextDataset(valid_text, tokenizer, args['max_text_lenth'] , valid_audios)
+valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size= args['batch_size'], shuffle=False)
+
+test_dataset = ContextDataset(test_text, tokenizer, args['max_text_lenth'] , test_audios)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size= args['batch_size'], shuffle=False)
+
+# audio_example = torch.randint(0, 10, (1, 8)) # B S
 
 model = Audio_Generator(target_vocab_size = 10, embed_dim = 100, decoder_nhead = 2, decoder_num_layers = 1)
-for i, embeddings in enumerate(train_loader):
+for i, batchi in enumerate(train_loader):
+    embeddings, audio = batchi
     print(embeddings)
-    print(model(embeddings, audio_example).shape)
+    print(model(embeddings, audio).shape)
     print(model.predict(embeddings))
     break
 
--- utils.py
+++ utils.py
@@ -1,24 +1,57 @@
-import os
-import sys
-import json
-import librosa
-import soundfile as sf
+# import os
+# import sys
+# import json
+# import soundfile as sf
+import pandas as pd
+import numpy as np
+from pretty_midi import PrettyMIDI
 
-ls = os.listdir
-jpath = os.path.join
 
-
-def read_json(path):
-    with open(path, 'r', encoding='utf8') as f:
-        data = f.read()
-        data = json.loads(data)
+def read_csv(path):
+    data = pd.read_csv(path)
     return data
 
 
-def save_json(data, path, sort=False):
-    with open(path, 'w', encoding='utf8') as f:
-        f.write(json.dumps(data, indent=4, sort_keys=sort, ensure_ascii=False))
+def get_midi_files(data, base_path):
+    midi_files = [base_path+i for i in data['midi_filename'].tolist()]
+    return midi_files
 
 
-def print_json(data):
-    print(json.dumps(data, indent=4, ensure_ascii=False))
+def get_text_descriptions(data):
+    text_descriptions = data['caption'].tolist()
+    return text_descriptions
+
+
+def GetNoteSequence(instrument) -> np.ndarray:
+    sorted_notes = sorted(instrument.notes, key=lambda x: x.start)
+    assert len(sorted_notes) > 0
+    notes = []
+
+    for note in sorted_notes:
+        notes.append([int(note.pitch), note.start, note.end])
+    # prev_start = sorted_notes[0].start
+    # for note in sorted_notes:
+    #     notes.append([note.pitch, note.start -
+    #                  prev_start, note.end-note.start])
+    #     prev_start = note.start
+    return notes, note.end
+
+
+# transform from [pitch,start,end] to [pitch] for each frame
+def midi_files_to_audios(midi_files, frame = 100):
+        frame = int(frame)
+        audios = []
+        max_end = 0
+
+        for f in midi_files:
+            pm = PrettyMIDI(f)
+            instrument = pm.instruments[0]
+            notes, end = GetNoteSequence(instrument)
+            max_end = max(max_end, end)
+            audios.append(notes)
+        output_audios = np.zeros([len(audios),int(max_end)*frame+frame])
+        for i,j in enumerate(audios):
+             for k in j:
+                output_audios[i][int(k[1]*frame):int(k[2]*frame)] = k[0]
+        return output_audios
+
