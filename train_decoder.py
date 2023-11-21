b695b45d2ebb0a608992f3620ec1c6fd8661503b
Pengbo Wei <68414598+Samwei1@users.noreply.github.com>
2023-11-21 22:54:52 +0800

    add file

---
+++ train_decoder.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import argparse
+import json
+import numpy as np
+import torch
+import torch.nn.functional as F
+import time
+import sys
+from torch.optim.lr_scheduler import LambdaLR
+import os
+from tqdm import tqdm
+from parse import parse_args
+from audio_decoder import Audio_Decoder
+from transformers import AdamW
+from utils import *
+from sklearn.metrics import accuracy_score  
+from sklearn.metrics import f1_score
+from dataset import ContextDataset
+from transformers import BertGenerationConfig, BertGenerationEncoder, EncoderDecoderModel, BertGenerationDecoder
+from third_party.constants import *
+from torch.utils.data import Dataset, DataLoader
+from dataset import create_epiano_datasets, compute_epiano_accuracy
+from transformers import Trainer, TrainingArguments,BertTokenizer, BertModel, BertPreTrainedModel, BertConfig
+from lr_scheduling import LrStepTracker
+if __name__ == '__main__':
+    args = parse_args()
+    base_path = args.base_path # './maestro-v2.0.0/'
+    csv_path = args.data_path + '/clean+GPTcaption.csv' # './data/clean+GPTcaption.csv'
+    processed_path = args.data_path + '/max_len{}_data.npy'.format(args.max_len)
+    print('generating dataset...')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cpu")
+
+    train_dataset, val_dataset, test_dataset = create_epiano_datasets("data", 2048)
+    print('Done...')
+    train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=5)
+    test_loader = DataLoader(test_dataset, batch_size=5)
+
+
+    model = Audio_Decoder(target_vocab_size = VOCAB_SIZE, embed_dim = 512, nhead = 8, num_layers = 6, device = device)
+    # model = MusicTransformer(n_layers=6, num_heads=8, d_model=512, dim_feedforward=1024,
+    #              dropout=0.1, max_sequence=2048)
+    model.to(device)
+    pth_path = '{}/decoder.pth'.format(args.model_path)
+    init_step = 0
+    lr_stepper = LrStepTracker(512, SCHEDULER_WARMUP_STEPS, init_step)
+    optimizer = AdamW(model.parameters(), lr= 1)
+    lr_scheduler = LambdaLR(optimizer, lr_stepper.step)
+    criterion = nn.CrossEntropyLoss(ignore_index=TOKEN_PAD)
+
+    print('starting')
+    best_epoch = 0
+    best_loss = float('inf')
+    best_acc = 0
+    metrics_update = 0
+    acc_list = []
+    for epoch in range(args.num_epochs):
+        model.train()
+        start_time = time.time()
+        t_loss = 0
+        train_num = 0 
+        pbar = tqdm(enumerate(train_loader), total=len(train_loader))
+        for i, batch in pbar:
+            optimizer.zero_grad()
+            x   = batch[0].to(device)
+            tgt = batch[1].to(device)
+            y = model(text_embed = None, audio = x, memory_key_padding_mask =None ,tgt_key_padding_mask=None)
+            # y = model(x)
+            y = y.reshape(y.shape[0] * y.shape[1], -1)
+            tgt = tgt.flatten()
+            loss = criterion(y, tgt)
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+            t_loss += loss.detach().item()
+            train_num += 1
+
+        print('epoch:', epoch)
+        print('train_loss {:.4f}'.format(t_loss/train_num) + " costs " + time.strftime(
+                            "%H: %M: %S", time.gmtime(time.time()-start_time)))
+        with torch.no_grad():
+            model.eval()
+            sum_acc    = 0.0
+            n_test     = len(val_loader)
+            for i, batchi in enumerate(val_loader):
+                x   = batch[0].to(device)
+                label = batch[1].to(device)
+                y = model(text_embed = None, audio = x, memory_key_padding_mask =None ,tgt_key_padding_mask=None)
+                # y = model(x)
+                sum_acc += float(compute_epiano_accuracy(y, label))
+
+            epoch_acc     = sum_acc / n_test
+            acc_list.append(epoch_acc)
+            print("[Valid]: ACC: {} - F1:".format(str(epoch_acc)))
+
+        if epoch_acc > best_acc: 
+                best_acc, best_epoch = epoch_acc, epoch
+                print("------------Best model, saving...------------")
+                if not os.path.exists(args.model_path):
+                    os.mkdir(args.model_path)
+                torch.save(model, pth_path)
+                print("------------Finished Save---------------------")
+
+        if epoch > best_epoch:
+            metrics_update += 1
+        else:
+            metrics_update = 0
+        # if metrics_update>=10:
+        #     break    
+    np.save('acc_log.npy', np.array(acc_list))
--- utils.py
+++ utils.py
@@ -5,12 +5,18 @@ import os
 import pandas as pd
 import numpy as np
 from pretty_midi import PrettyMIDI
-
+import pretty_midi
+from third_party.processor import encode_midi, decode_midi
+from third_party.constants import *
+SEQUENCE_START = 0
+import random 
 
 def read_csv(path):
     data = pd.read_csv(path)
     return data
 
+def cpu_device():
+    return torch.device("cpu")
 
 def get_midi_files(data, base_path):
     midi_files = [base_path+i for i in data['midi_filename'].tolist()]
@@ -64,6 +70,79 @@ def midi_files_to_audios(midi_files, frame = 100):
             output_audios[i][int(k[2]*frame)+1:] = pad_idx
         return output_audios, padding_masks
 
+def process_midi(raw_mid, max_seq, random_seq):
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Takes in pre-processed raw midi and returns the input and target. Can use a random sequence or
+    go from the start based on random_seq.
+    ----------
+    """
+
+    x   = np.full((max_seq, ), TOKEN_PAD)
+    tgt = np.full((max_seq, ), TOKEN_PAD)
+
+    raw_len     = len(raw_mid)
+    full_seq    = max_seq + 1 # Performing seq2seq
+
+    if(raw_len == 0):
+        return x, tgt
+
+    if(raw_len < full_seq):
+        x[:raw_len]         = raw_mid
+        tgt[:raw_len-1]     = raw_mid[1:]
+        tgt[raw_len-1]      = TOKEN_END
+    else:
+        # Randomly selecting a range
+        if(random_seq):
+            end_range = raw_len - full_seq
+            start = random.randint(SEQUENCE_START, end_range)
+
+        # Always taking from the start to as far as we can
+        else:
+            start = SEQUENCE_START
+
+        end = start + full_seq
+
+        data = raw_mid[start:end]
+
+        x = data[:max_seq]
+        tgt = data[1:full_seq]
+
+    return x, tgt
+
+
+
+def midi_files_to_audios_sam(midi_files, max_seq = 2048, random_seq=True):
+        audios = []
+        labels = []
+        padding_mask = []
+        for f in midi_files:
+            raw_mid = encode_midi(f)
+            x, tgt = process_midi(raw_mid, max_seq, random_seq)
+            audios.append(x)
+            labels.append(tgt)
+
+        return audios, labels
+
+def load_data_sam(processed_path, csv_path, base_path, max_seq):
+    if not os.path.exists(processed_path):
+        print('processing data...')
+        data = read_csv(csv_path)
+        midi_files = get_midi_files(data, base_path)
+        text_descriptions = get_text_descriptions(data)
+        audios, labels = midi_files_to_audios_sam(midi_files, max_seq)
+        data_dict = {'text_descriptions':text_descriptions, 'audios':audios, 'labels':labels}
+        np.save(processed_path, data_dict)
+    else:
+        print('loading data...')
+        data_dict = np.load(processed_path, allow_pickle=True).item()
+        text_descriptions, audios, labels = data_dict['text_descriptions'], data_dict['audios'], data_dict['labels']
+        # print(np.sum(audios[0][:int(np.sum(padding_masks[0]))] == 0), np.sum(padding_masks[0]) )
+    return text_descriptions, np.array(audios), np.array(labels)
+
+
 def load_data(processed_path, csv_path, base_path, frame):
     if not os.path.exists(processed_path):
         print('processing data...')
@@ -99,3 +178,37 @@ def split_train_val_test(text_descriptions, audios, padding_masks, train_ratio =
     test_padding_masks = padding_masks[train_num+valid_num:]
 
     return train_text, valid_text, test_text, train_audios, valid_audios, test_audios, train_padding_masks, valid_padding_masks, test_padding_masks
+
+
+
+def audios_to_midi_files(frames, index, frame = 100, write_path = './output_midi/'):
+        # 128 as start index, 129 as end index
+        start_idx = 129
+        end_idx = 130
+        pad_idx = 131
+        frame = int(frame)
+        if not os.path.exists(write_path):
+            os.mkdir(write_path)
+        count = 0
+        for mi in frames:
+            music = PrettyMIDI()
+            piano_program = pretty_midi.instrument_name_to_program(
+            'Acoustic Grand Piano')
+            piano = pretty_midi.Instrument(program=piano_program)
+            t = 0
+            mi = mi.cpu().numpy()
+            for pitch in mi:
+                pitch = int(pitch)
+                if pitch < 129 and pitch > 0:
+                    note = pretty_midi.Note(
+                        velocity=100, pitch=pitch-1, start=t, end=t + 1/frame)
+                    t += 1/frame
+                    piano.notes.append(note)
+                elif pitch == end_idx:
+                    break
+                else:
+                    t += 1/frame
+
+            music.instruments.append(piano)
+            music.write(write_path + 'test{}.mid'.format(index+count))
+            count += 1
