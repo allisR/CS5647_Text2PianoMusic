6bf4d7405073c7947ba155ee11659a9988d9d8fa
Pengbo Wei <68414598+Samwei1@users.noreply.github.com>
2023-11-21 22:49:28 +0800

    modify decoder

--- audio_decoder.py
+++ audio_decoder.py
@@ -2,59 +2,99 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import math
+from third_party.constants import *
+import random
 # Define the Audio_Decoder model
 class Audio_Decoder(nn.Module):
-    def __init__(self, target_vocab_size, embed_dim, nhead, num_layers):
+    def __init__(self, target_vocab_size, embed_dim, nhead, num_layers, device):
         super(Audio_Decoder, self).__init__()
         self.d_model = embed_dim
         self.embedding_encoder = nn.Embedding(target_vocab_size, embed_dim)
         # self.decoder = nn.LSTM(embed_dim, embed_dim)
-        self.pos_decoder = PositionalEncoding(d_model=embed_dim)
-        decoder_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead)
+        self.pos_decoder = PositionalEncoding(d_model=embed_dim, max_len = 2048)
+        decoder_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead, dim_feedforward = 1024)
         self.decoder = nn.TransformerDecoder(decoder_layer, num_layers=num_layers)
         self.out = nn.Linear(embed_dim, target_vocab_size)
         self.src_mask = None
-        self.init_weights()
+        # self.init_weights()
+        self.device = device
+        self.softmax = nn.Softmax(dim=-1)
 
-    def init_weights(self):
-        initrange = 0.1
-        self.embedding_encoder.weight.data.uniform_(-initrange, initrange)
-        self.out.bias.data.zero_()
-        self.out.weight.data.uniform_(-initrange, initrange)
+    # def init_weights(self):
+    #     initrange = 1.0
+    #     self.embedding_encoder.weight.data.uniform_(-initrange, initrange)
+    #     self.out.bias.data.zero_()
+    #     self.out.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, text_embed, audio, memory_key_padding_mask, tgt_key_padding_mask):
-        audio = torch.permute(audio, (1,0))
-        audio_embed = self.embedding_encoder(audio)* math.sqrt(self.d_model)
-        S, B, E =  audio_embed.shape
+        audio_embed = self.embedding_encoder(audio)
+        audio_embed =  audio_embed.permute(1,0,2)
         audio_embed = self.pos_decoder(audio_embed)
-        
+        device = audio.device
+        S, B, E = audio_embed.shape
         if self.src_mask is None or self.src_mask.size(0) != S:
-            device = audio.device
             self.src_mask = nn.Transformer.generate_square_subsequent_mask(S).to(device)
 
-        decoder_h_seq = self.decoder(tgt = audio_embed, memory = text_embed, tgt_mask = self.src_mask, 
-                                     tgt_key_padding_mask = tgt_key_padding_mask.type(torch.int64),
-                                     memory_key_padding_mask = memory_key_padding_mask) # seq batch embed [S, B, E]
+        if tgt_key_padding_mask is None:
+            # memory_key_padding_mask = ~memory_key_padding_mask.type(torch.bool)
+            if text_embed is None:
+                decoder_h_seq = self.decoder(tgt = audio_embed, memory = torch.ones(1, B, E).to(self.device), tgt_mask = self.src_mask)
+            else:
+                decoder_h_seq = self.decoder(tgt = audio_embed, memory = text_embed, tgt_mask = self.src_mask)
+        else:
+            decoder_h_seq = self.decoder(tgt = audio_embed, memory = text_embed, tgt_mask = self.src_mask, 
+                                     tgt_key_padding_mask = tgt_key_padding_mask.type(torch.bool),
+                                     memory_key_padding_mask = ~memory_key_padding_mask.type(torch.bool)) # seq batch embed [S, B, E]
         # decoder_h_seq = self.decoder(audio_embed, self.src_mask) # seq batch embed [S, B, E]
         decoder_h_seq = torch.permute(decoder_h_seq, (1,0,2))
         output = self.out(decoder_h_seq)
         return output
 
-    def predict(self, text_embed, memory_key_padding_mask):
-        start = torch.tensor([[129]]).type(torch.LongTensor)
-        while True:
-            audio_embed = self.embedding_encoder(start)
-            audio_embed = self.pos_decoder(audio_embed)
-            audio_embed = torch.permute(audio_embed, (1, 0, 2))
+    def predict_decoder(self, primer, target_seq_length=1024):
+        gen_seq = torch.full((1,target_seq_length), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=torch.device('cuda:0'))
+        num_primer = len(primer)
+        gen_seq[..., :num_primer] = primer.type(TORCH_LABEL_TYPE).to(torch.device('cuda:0'))
+        cur_i = num_primer
+        while(cur_i < target_seq_length):
+            y = self.softmax(self.forward(text_embed = None, memory_key_padding_mask = None, tgt_key_padding_mask = None, audio = gen_seq[..., :cur_i]))[..., :TOKEN_END]
+            token_probs = y[:, cur_i-1, :]
+            distrib = torch.distributions.categorical.Categorical(probs=token_probs)
+            next_token = distrib.sample()
+            # print("next token:",next_token)
+            gen_seq[:, cur_i] = next_token
 
-            decoder_h_seq = self.decoder(audio_embed, text_embed, memory_key_padding_mask=memory_key_padding_mask)
-            decoder_h_seq = torch.permute(decoder_h_seq, (1, 0, 2))
-            output = self.out(decoder_h_seq)
-            output = torch.argmax(output, dim=2)[0][-1].reshape(1,-1)
-            start = torch.cat([start, output], 1)
-            if start[0][-1].item() == 130:
+
+            # Let the transformer decide to end if it wants to
+            if(next_token == TOKEN_END):
+                print("Model called end of sequence at:", cur_i, "/", target_seq_length)
                 break
-        return start
+            cur_i += 1
+            if(cur_i % 50 == 0):
+                print(cur_i, "/", target_seq_length)
+        return gen_seq[:, :cur_i]
+
+
+    def predict(self, text_embed, memory_key_padding_mask, device, target_seq_length=1024):
+        ix = random.randrange(128)
+        gen_seq = torch.full((1,target_seq_length), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=torch.device('cuda:0'))
+        gen_seq[:, :1] = ix
+        cur_i = 1
+        while(cur_i < target_seq_length):
+            y = self.softmax(self.forward(text_embed = text_embed, memory_key_padding_mask = memory_key_padding_mask, tgt_key_padding_mask = None, audio = gen_seq[..., :cur_i]))[..., :TOKEN_END]
+            token_probs = y[:, cur_i-1, :]
+            distrib = torch.distributions.categorical.Categorical(probs=token_probs)
+            next_token = distrib.sample()
+            # print("next token:",next_token)
+            gen_seq[:, cur_i] = next_token
+
+            # Let the transformer decide to end if it wants to
+            if(next_token == TOKEN_END):
+                print("Model called end of sequence at:", cur_i, "/", target_seq_length)
+                break
+            cur_i += 1
+            if(cur_i % 50 == 0):
+                print(cur_i, "/", target_seq_length)
+        return gen_seq[:, :cur_i]
 
 
 class PositionalEncoding(nn.Module):
@@ -77,3 +117,6 @@ class PositionalEncoding(nn.Module):
         """
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
+
+
+
